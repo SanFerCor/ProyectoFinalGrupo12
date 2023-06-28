f8e131d41d65105256cf5050d626f38c12d22682
Leonel Chavez <leonel.chavez@openhub.pe>
2023-06-27 19:04:29 -0500

    fix: solucion

--- App.eShop/src/app/Home/detail/index-detail.component.ts
+++ App.eShop/src/app/Home/detail/index-detail.component.ts
@@ -1,34 +1,65 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { tap } from "rxjs";
-import { Product } from "../../shared/models/product.model";
-import { ProductService } from "../../shared/services/product.service";
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tap } from 'rxjs';
+import { Product } from '../../shared/models/product.model';
+import { ProductService } from '../../shared/services/product.service';
+import * as _ from 'lodash';
 
 @Component({
   templateUrl: './index-detail.component.html',
-  styleUrls: ['./index-detail.component.scss']
+  styleUrls: ['./index-detail.component.scss'],
 })
 export class IndexDetailComponent implements OnInit {
   products: Product[];
-  searchControl = new FormControl("");
 
-  constructor(private readonly productSvc: ProductService) {
+  searchControl = new FormControl('');
 
+  basket: { product: Product; quantity: number; price: number }[] = [];
+
+  get totalAmount() {
+    return _.sumBy(this.basket, (x) => x.price);
   }
+
+  constructor(private readonly productSvc: ProductService) {}
 
   ngOnInit(): void {
-    this.productSvc.list().pipe(tap((products) => {
-      this.products = products;
-    })).subscribe();
-
-    this.searchControl.valueChanges.pipe(
-      tap((value) => {
-        this.productSvc.list(value).pipe(tap((products) => {
+    this.productSvc
+      .list()
+      .pipe(
+        tap((products) => {
           this.products = products;
-        })).subscribe();
-      })
-    ).subscribe();
+        })
+      )
+      .subscribe();
+
+    this.searchControl.valueChanges
+      .pipe(
+        tap((value) => {
+          this.productSvc
+            .list(value)
+            .pipe(
+              tap((products) => {
+                this.products = products;
+              })
+            )
+            .subscribe();
+        })
+      )
+      .subscribe();
   }
 
+  addProduct(product: Product) {
+    const item = this.basket.find((x) => x.product.code === product.code);
+
+    if (item) {
+      item.quantity += 1;
+      item.price = item.quantity * item.product.price;
+    } else {
+      this.basket.push({ product, price: product.price, quantity: 1 });
+    }
+  }
+
+  removeProduct(item: (typeof this.basket)[0]) {
+    _.remove(this.basket, (x) => x.product.code === item.product.code);
+  }
 }
